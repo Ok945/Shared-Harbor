effebb7c8fbe826a1b2465ac45658217e0dd942a
Ok945 <omkendre5454@gmail.com>
2024-04-04 17:52:03 +0530

    Home page and Header is added

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,9 +1,150 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Header.css'
+import { Link, useNavigate } from 'react-router-dom'
+import OlxLogo from "./icons/OlxLogo";
+import SearchIcon from "./icons/SearchIcon";
+import Arrow from "./icons/Arrow";
+import SellButton from "./icons/SellButton";
+import SellButtonPlus from "./icons/SellButtonPlus";
+import { jwtDecode } from 'jwt-decode';
+
 
 const Header = () => {
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    // Retrieve token from localStorage
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      // Decode the token
+      const decodedToken = jwtDecode(token);
+
+      // Extract user data from decoded token
+      const { data } = decodedToken;
+
+      // Set user data state
+      // console.log(data.email);
+      // console.log(data.fname);
+      setUser(data.fname);
+    }
+  }, []);
+
+
+  const [wordEntered, setWordEntered] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+
+  const handleEmptyClick = () => {
+    alert("No items found.., please search by product name");
+  }
+
+
+
+  const handleFilter = (event) => {
+
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+
+
+  const handleSelectedSearch = (value) => {
+
+
+  }
+
+
+
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    setUser("");
+    navigate('/')
+  }
+
   return (
-    <div>
-      
+    <div className="headerParentDiv">
+      <div className="headerChildDiv">
+        <Link to="/">
+          <div className="brandName">
+            <OlxLogo></OlxLogo>
+          </div>
+        </Link>
+        <div className="placeSearch">
+          <input type="text"
+            placeholder="Search specific product..."
+            value={wordEntered}
+            onChange={handleFilter}
+          />{filteredData.length === 0 ? (
+            <div onClick={handleEmptyClick}> <SearchIcon /> </div>
+          ) : (
+            <div id="clearBtn" onClick={clearInput} > <Arrow></Arrow></div>
+          )}
+          {filteredData.length !== 0 && (
+            <div className="dataResult-header">
+              {filteredData.slice(0, 15).map((value, key) => {
+                return (
+                  <div key={key} className="dataItem-header" onClick={() => handleSelectedSearch(value)}>
+                    <p>{value.name} </p>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+
+        </div>
+
+
+
+        <div className="productSearch">
+          {/* <Search /> */}
+        </div>
+
+        <div className="loginPage">
+          {user ? (
+            user
+          ) : (
+            <Link to="/login">
+              <span>Login</span>
+            </Link>
+          )}
+          <hr />
+        </div>
+
+        {user && (
+          <span onClick={logoutHandler} className="logout-span">
+            Logout
+          </span>
+        )}
+
+        {localStorage.getItem('token') ? (
+          <Link to="/add-product">
+            <div className="sellMenu">
+              <SellButton></SellButton>
+              <div className="sellMenuContent">
+                <SellButtonPlus></SellButtonPlus>
+                <span>SELL</span>
+              </div>
+            </div>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <div className="sellMenu">
+              <SellButton></SellButton>
+              <div className="sellMenuContent">
+                <SellButtonPlus></SellButtonPlus>
+                <span>SELL</span>
+              </div>
+            </div>
+          </Link>
+        )}
+      </div>
     </div>
   )
 }
--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import { Link, useNavigate } from 'react-router-dom';
+import ProductCard from './ProductCard';
+import axios from 'axios';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const navigate = useNavigate();
+
+
+    const [products, setProducts] = useState([]);
+
+
+    // Function to check if user is logged in
+    const checkLoggedIn = () => {
+        if (!localStorage.getItem('token')) {
+
+            navigate('/login');
+        } else {
+
+            navigate('/add-product');
+        }
+    };
+
+
+    useEffect(() => {
+
+        const url = 'http://localhost:8080/getProduct'
+
+        axios.get(url)
+            .then((res) => {
+                // console.log(res);
+                if (res.data.products) {
+                    setProducts(res.data.products);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("server err!!")
+            })
+
+    })
+
+    return (
+        <div className='mainContainer'>
+            <Header />
+            {products && products.length > 0 &&
+                <div className='productsContainer'>
+                    {products.map((product) => ( // Iterate over products array and pass each product to ProductCard
+                        <ProductCard key={product._id} product={product} />
+                    ))}
+                </div>
+            }
+        </div>
+    )
 }
 
 export default Home
